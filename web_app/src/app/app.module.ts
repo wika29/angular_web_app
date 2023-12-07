487b36511285a0ada6ecbc951864431693e50221
ADESI\lvonhaef <lukas.von.haefen@adp.com>
2023-12-07 11:11:54 +0100

    Steckbrief implementierung 1

--- web_app/src/app/app.module.ts
+++ web_app/src/app/app.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {InitialScreenComponent} from './screens/initial_screen/initial-screen.component';
+// import {InitialScreenComponent} from './screens/initial_screen/initial-screen.component';
+import { SteckbriefComponent } from './Components/Steckbrief/steckbrief.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InitialScreenComponent
+    // InitialScreenComponent,
+    SteckbriefComponent
   ],
   imports: [
     BrowserModule
