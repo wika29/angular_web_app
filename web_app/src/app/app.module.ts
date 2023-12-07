5a75cc121bcbc97fb2124600b73dffe5064237ec
Jasha Meusel <j.meusel@neusta.de>
2023-12-07 08:35:14 +0100

    feat(main): init structure

--- web_app/src/app/app.module.ts
+++ web_app/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {InitialScreenComponent} from './screens/initial_screen/initial-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialScreenComponent
   ],
   imports: [
     BrowserModule
