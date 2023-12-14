f41136f8d43e0c946a8b558d4ae805847dd1709d
Wiktoria Wachowiak <wiktoria.wachowiak@asp.com>
2023-12-14 11:11:56 +0100

    implement backend

--- web_app/src/app/app.component.ts
+++ web_app/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {DialogComponent} from "./dialog-popup/dialog/dialog.component";
+
 
 @Component({
   selector: 'app-root',
@@ -13,4 +16,18 @@ export class AppComponent {
   openOverlay() {
       this.showOverlay = !this.showOverlay;
   }
+
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      data: { /* opcjonalne dane przekazywane do dialogu */ }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
 }
--- web_app/src/app/app.module.ts
+++ web_app/src/app/app.module.ts
@@ -3,14 +3,21 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {InitialScreenComponent} from './screens/initial_screen/initial-screen.component';
+import {OptionButton} from "../widgets/steckbrief_button/option_button.component";
+import { DialogComponent } from './dialog-popup/dialog/dialog.component';
+import {MatDialogModule} from '@angular/material/dialog';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InitialScreenComponent
-  ],
+    declarations: [
+        AppComponent,
+        InitialScreenComponent,
+        OptionButton,
+        DialogComponent
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
