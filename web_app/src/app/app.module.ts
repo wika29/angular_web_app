ace891caeccefb5e586fbbb13362d72761941252
ADESI\lvonhaef <lukas.von.haefen@adp.com>
2024-01-18 08:30:16 +0100

    theme Hinzugefügt

--- web_app/src/app/app.module.ts
+++ web_app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import {FormsModule} from "@angular/forms";
 import {SizedBoxComponent} from './Components/Button/sized-box/sized-box.component';
 import {SearchFieldComponent} from './Components/Button/search-field/search-field.component';
 import {SteckbriefComponent} from './Components/Steckbrief/steckbrief.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import {SteckbriefComponent} from './Components/Steckbrief/steckbrief.component'
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
